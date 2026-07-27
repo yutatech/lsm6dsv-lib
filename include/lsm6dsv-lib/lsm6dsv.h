511b44e1d8a96ad3b6d7b99f160abb93f5340ae1
Yuta Fujiyama <yuta.technology@gmail.com>
2026-07-27 20:53:45 +0900

    initial commit

---
+++ include/lsm6dsv-lib/lsm6dsv.h
@@ -0,0 +1,305 @@
+#pragma once
+
+#include <cstddef>
+
+#include "lsm6dsv-lib/lsm6dsv_register.h"
+
+namespace lsm6dsv_lib {
+class LSM6DSV {
+ public:
+  LSM6DSV(void (*spi_transmit_and_receive)(uint8_t* tx_buf, uint8_t* rx_buf,
+                                           size_t length),
+          void (*enable_cs)(void) = nullptr, void (*disable_cs)(void) = nullptr)
+      : enable_cs(enable_cs),
+        disable_cs(disable_cs),
+        spi_transmit_and_receive(spi_transmit_and_receive) {};
+  ~LSM6DSV() = default;
+
+  /**
+   * @brief Initializes the LSM6DSV sensor.
+   * @return true if initialization is successful, false otherwise.
+   */
+  bool Init();
+
+  /**
+   * @brief Read accelerometer data.
+   *
+   * @param x Pointer to store the x-axis acceleration.
+   * @param y Pointer to store the y-axis acceleration.
+   * @param z Pointer to store the z-axis acceleration.
+   * @return true if reading is successful, false if reading fails.
+   */
+  bool ReadAccel(float* x, float* y, float* z);
+
+  /**
+   * @brief Read gyroscope data.
+   *
+   * @param x Pointer to store the x-axis gyroscope data.
+   * @param y Pointer to store the y-axis gyroscope data.
+   * @param z Pointer to store the z-axis gyroscope data.
+   * @return true if reading is successful, false if reading fails.
+   */
+  bool ReadGyro(float* x, float* y, float* z);
+
+  /**
+   * @brief Write data to one specific register.
+   *
+   * @param address The register address to write to.
+   * @param data Pointer to the data to write.
+   * @return true if writing is successful, false if writing fails.
+   */
+  bool WriteRegister(Register address, uint8_t* data);
+
+  /**
+   * @brief Read data from one specific register.
+   *
+   * @param address The register address to read from.
+   * @param data Pointer to store the read data.
+   * @return true if reading is successful, false if reading fails.
+   */
+  bool ReadRegister(Register address, uint8_t* data);
+
+  /**
+   * @brief Write data to multiple registers starting from a specific address.
+   *
+   * @param start_address The register address to start writing from.
+   * @param data Pointer to the data to write.
+   * @param length The number of registers to write.
+   * @return true if writing is successful, false if writing fails.
+   */
+  bool WriteRegisters(Register start_address, uint8_t* data, size_t length);
+
+  /**
+   * @brief Read data from multiple registers starting from a specific address.
+   *
+   * @param start_address The register address to start reading from.
+   * @param data Pointer to store the read data.
+   * @param length The number of registers to read.
+   * @return true if reading is successful, false if reading fails.
+   */
+  bool ReadRegisters(Register start_address, uint8_t* data, size_t length);
+
+  /**
+   * @brief Read the WHO_AM_I register to verify the device identity.
+   *
+   * @return The value of the WHO_AM_I register.
+   */
+  uint8_t WhoAmI();
+
+  enum class EnableState : bool {
+    kEnabled = true,
+    kDisabled = false,
+  };
+
+  /**
+   * @brief Write the I3C enabled state to the register.
+   *        When using SPI or I2c, this should be set to kDisabled.
+   *
+   * @param enable_state The state to enable or disable I3C.
+   * @return true if the operation is successful, false if operation fails.
+   */
+  bool WriteI3cEnabled(EnableState enable_state);
+
+  /**
+   * @brief Write the auto-increment enabled state to the register.
+   *        kEnabled for reading or writing multiple registers in a row,
+   *
+   * @param enable_state The state to enable or disable auto-increment.
+   * @return true if the operation is successful.
+   * @return false if the operation fails.
+   */
+  bool WriteAutoIncrementEnabled(EnableState enable_state);
+
+  enum class GyroDataRate : uint8_t {
+    kPowerDown = 0b0000,
+    k12_5Hz = 0b0001,
+    k26Hz = 0b0010,
+    k52Hz = 0b0011,
+    k104Hz = 0b0100,
+    k208Hz = 0b0101,
+    k416Hz = 0b0110,
+    k833Hz = 0b0111,
+    k1660Hz = 0b1000,
+    k3330Hz = 0b1001,
+    k6660Hz = 0b1010,
+  };
+
+  /**
+   * @brief Write the gyroscope data rate to the register.
+   *
+   * @param data_rate The desired gyroscope data rate.
+   * @return true if the operation is successful, false if operation fails.
+   */
+  bool WriteGyroDataRate(GyroDataRate data_rate);
+
+  enum class GyroFullScale : uint8_t {
+    k125dps = 0b0010,
+    k250dps = 0b0000,
+    k500dps = 0b0100,
+    k1000dps = 0b1000,
+    k2000dps = 0b1100,
+    k4000dps = 0b0001,
+  };
+
+  /**
+   * @brief Write the gyroscope full scale to the register.
+   *
+   * @param full_scale The desired gyroscope full scale.
+   * @return true if the operation is successful, false if operation fails.
+   */
+  bool WriteGyroFullScale(GyroFullScale full_scale);
+
+  /**
+   * @brief Read the gyroscope full scale from the register.
+   *
+   * @return The current gyroscope full scale setting.
+   */
+  GyroFullScale ReadGyroFullScale();
+
+  /**
+   * @brief Configure the gyroscope sensitivity based on the full scale setting.
+   *
+   * @param full_scale The full scale setting to configure sensitivity.
+   */
+  void ConfigureGyroSensitivity(GyroFullScale full_scale);
+
+  enum class AccelDataRate : uint8_t {
+    kPowerDown = 0b0000,
+    k1_6Hz = 0b1011,
+    k12_5Hz = 0b0001,
+    k26Hz = 0b0010,
+    k52Hz = 0b0011,
+    k104Hz = 0b0100,
+    k208Hz = 0b0101,
+    k416Hz = 0b0110,
+    k833Hz = 0b0111,
+    k1660Hz = 0b1000,
+    k3330Hz = 0b1001,
+    k6660Hz = 0b1010,
+  };
+
+  /**
+   * @brief Write the accelerometer data rate to the register.
+   *
+   * @param data_rate The desired accelerometer data rate.
+   * @return true if the operation is successful, false if operation fails.
+   */
+  bool WriteAccDataRate(AccelDataRate data_rate);
+
+  enum class AccelFullScale : uint8_t {
+    k2g = 0b00,
+    k4g = 0b10,
+    k8g = 0b11,
+    k16g = 0b01,
+  };
+
+  /**
+   * @brief Write the accelerometer full scale to the register.
+   *
+   * @param full_scale The desired accelerometer full scale.
+   * @return true if the operation is successful.
+   * @return false if the operation fails.
+   */
+  bool WriteAccFullScale(AccelFullScale full_scale);
+
+  /**
+   * @brief Read the accelerometer full scale from the register.
+   *
+   * @return The current accelerometer full scale setting.
+   */
+  AccelFullScale ReadAccFullScale();
+
+  /**
+   * @brief Configure the accelerometer sensitivity based on the full scale
+   *        setting.
+   *
+   * @param full_scale The full scale setting to configure sensitivity.
+   */
+  void ConfigureAccSensitivity(AccelFullScale full_scale);
+
+  /**
+   * @brief Read accelerometer and gyroscope data.
+   *
+   * @param gyro_x pointer to store the x-axis gyroscope data.
+   * @param gyro_y pointer to store the y-axis gyroscope data.
+   * @param gyro_z pointer to store the z-axis gyroscope data.
+   * @param acc_x pointer to store the x-axis acceleration data.
+   * @param acc_y pointer to store the y-axis acceleration data.
+   * @param acc_z pointer to store the z-axis acceleration data.
+   * @return true if the operation is successful, false if operation fails.
+   */
+  bool ReadAccAndGyro(float& gyro_x, float& gyro_y, float& gyro_z, float& acc_x,
+                      float& acc_y, float& acc_z);
+
+  /**
+   * @brief Reset the memory of the LSM6DSV device. All registers will be set to
+   *        their default values. You sould wait for 15ms after this.
+   * @return true if the reset is successful, false if it fails.
+   */
+  bool ResetMemory();
+
+  /**
+   * @brief Reboot the LSM6DSV device. You should wait for 50us after this.
+   * @return true if the reboot is successful, false if it fails.
+   */
+  bool RebootDevice();
+
+  // Interrupt enable configuration
+  bool WriteInt1Enable(uint8_t value);
+  bool WriteInt2Enable(uint8_t value);
+  bool ReadInt1Enable(uint8_t& value);
+  bool ReadInt2Enable(uint8_t& value);
+
+  // SFLP (Sensor Fusion Low Power) enable
+  bool WriteSflpEnable(EnableState enable_state);
+
+  // SFLP output selection
+  enum class SflpOutputSel : uint8_t {
+    kGyro = 0,
+    kAccel = 1,
+    kQuaternion = 2,
+    kGravity = 3,
+  };
+  bool WriteSflpOutputSel(SflpOutputSel sel);
+
+  // SFLP data rate
+  enum class SflpDataRate : uint8_t {
+    k15Hz = 0b000,
+    k30Hz = 0b001,
+    k60Hz = 0b010,
+    k120Hz = 0b011,
+    k240Hz = 0b100,
+    k480Hz = 0b101,
+  };
+  bool WriteSflpDataRate(SflpDataRate data_rate);
+
+  // Read Quaternion data
+  bool ReadQuaternion(float& w, float& x, float& y, float& z);
+
+  // Read Gravity vector data
+  bool ReadGravity(float& x, float& y, float& z);
+
+  // Read Gyro Bias data
+  bool ReadGyroBias(float& x, float& y, float& z);
+
+  // Helper APIs to enable BDU, timestamp, rounding, and FIFO
+  bool EnableBlockDataUpdate(EnableState enable_state);
+  bool WriteTimestamp(EnableState enable_state);
+  bool EnableTimestampRounding(EnableState enable_state);
+
+  // bool WriteIntEnable(uint8_t int_enable_mask);
+  // bool WriteSflpEnabled(EnableState enable_state);
+  // bool WriteSflpOutput(SflpOutput output);
+
+
+ private:
+  float acc_sensitivity_ = 0.0f;
+  float gyro_sensitivity_ = 0.0f;
+
+  void (*enable_cs)(void);
+  void (*disable_cs)(void);
+  void (*spi_transmit_and_receive)(uint8_t* tx_buf, uint8_t* rx_buf,
+                                   size_t length);
+};
+
+}  // namespace lsm6dsv_lib
---
+++ src/lsm6dsv.cc
@@ -0,0 +1,417 @@
+#include "lsm6dsv-lib/lsm6dsv.h"
+
+#include "lsm6dsv-lib/lsm6dsv_register.h"
+
+namespace lsm6dsv_lib {
+
+bool LSM6DSV::Init() {
+  bool is_success = true;
+  is_success &= WriteI3cEnabled(LSM6DSV::EnableState::kDisabled);
+  is_success &= WriteAutoIncrementEnabled(LSM6DSV::EnableState::kEnabled);
+  is_success &= EnableBlockDataUpdate(LSM6DSV::EnableState::kEnabled);
+  is_success &= WriteTimestamp(LSM6DSV::EnableState::kEnabled);
+  is_success &= EnableTimestampRounding(LSM6DSV::EnableState::kEnabled);
+  return is_success;
+}
+
+bool LSM6DSV::ReadAccel(float* x, float* y, float* z) {
+  return true;  // or false on failure
+}
+
+bool LSM6DSV::ReadGyro(float* x, float* y, float* z) {
+  return true;  // or false on failure
+}
+
+bool LSM6DSV::WriteRegister(Register address, uint8_t* data) {
+  uint8_t tx_buf[2];
+  uint8_t rx_buf[2];
+
+  tx_buf[0] = static_cast<uint8_t>(address);
+  tx_buf[1] = *data;
+
+  if (enable_cs) { enable_cs(); }
+  spi_transmit_and_receive(tx_buf, rx_buf, 2);
+  if (disable_cs) { disable_cs(); }
+
+  return true;
+}
+
+bool LSM6DSV::ReadRegister(Register address, uint8_t* data) {
+  uint8_t tx_buf[2];
+  uint8_t rx_buf[2];
+
+  tx_buf[0] = static_cast<uint8_t>(address) | 0b10000000;
+  tx_buf[1] = 0;
+
+  if (enable_cs) { enable_cs(); }
+  spi_transmit_and_receive(tx_buf, rx_buf, 2);
+  if (disable_cs) { disable_cs(); }
+
+  *data = rx_buf[1];
+
+  return true;
+}
+
+bool LSM6DSV::WriteRegisters(Register start_address, uint8_t* data,
+                             size_t length) {
+  uint8_t tx_buf[length + 1];
+  uint8_t rx_buf[length + 1];
+
+  tx_buf[0] = static_cast<uint8_t>(start_address);
+  for (size_t i = 0; i < length; ++i) { tx_buf[i + 1] = data[i]; }
+
+  if (enable_cs) { enable_cs(); }
+  spi_transmit_and_receive(tx_buf, rx_buf, length + 1);
+  if (disable_cs) { disable_cs(); }
+
+  return true;
+}
+
+bool LSM6DSV::ReadRegisters(Register start_address, uint8_t* data,
+                            size_t length) {
+  uint8_t tx_buf[length + 1];
+  uint8_t rx_buf[length + 1];
+  tx_buf[0] = static_cast<uint8_t>(start_address) | 0b10000000;
+
+  if (enable_cs) { enable_cs(); }
+  spi_transmit_and_receive(tx_buf, rx_buf, length + 1);
+  if (disable_cs) { disable_cs(); }
+
+  for (size_t i = 0; i < length; ++i) { data[i] = rx_buf[i + 1]; }
+
+  return true;
+}
+
+uint8_t LSM6DSV::WhoAmI() {
+  uint8_t who_am_i;
+  if (ReadRegister(Register::WHO_AM_I, &who_am_i)) {
+    // Ideally 0x6B
+    // std::cout << "Who Am I: 0x" << std::hex << static_cast<int>(who_am_i)
+    //           << std::dec << std::endl;
+    return who_am_i;
+  } else {
+    return 0;
+  }
+}
+
+bool LSM6DSV::WriteI3cEnabled(EnableState enable_state) {
+  uint8_t ctrl9_xl;
+  ReadRegister(Register::CTRL9_XL, &ctrl9_xl);
+
+  if (enable_state == EnableState::kEnabled) {
+    ctrl9_xl |= 0b00000001;  // Set the I3C_EN bit
+  } else {
+    ctrl9_xl &= ~0b00000001;  // Clear the I3C_EN bit
+  }
+
+  return WriteRegister(Register::CTRL9_XL, &ctrl9_xl);
+}
+
+bool LSM6DSV::WriteAutoIncrementEnabled(EnableState enable_state) {
+  uint8_t ctrl3_c;
+  ReadRegister(Register::CTRL3_C, &ctrl3_c);
+
+  if (enable_state == EnableState::kEnabled) {
+    ctrl3_c |= 0b00000100;  // Set the IF_INC bit
+  } else {
+    ctrl3_c &= ~0b00000100;  // Clear the IF_INC bit
+  }
+
+  return WriteRegister(Register::CTRL3_C, &ctrl3_c);
+}
+
+bool LSM6DSV::EnableBlockDataUpdate(EnableState enable_state) {
+  uint8_t ctrl3_c;
+  ReadRegister(Register::CTRL3_C, &ctrl3_c);
+
+  if (enable_state == EnableState::kEnabled) {
+    ctrl3_c |= 0b01000000;  // Set the BDU bit
+  } else {
+    ctrl3_c &= ~0b01000000;  // Clear the BDU bit
+  }
+
+  return WriteRegister(Register::CTRL3_C, &ctrl3_c);
+}
+
+bool LSM6DSV::WriteTimestamp(EnableState enable_state) {
+  uint8_t ctrl10_c;
+  ReadRegister(Register::CTRL10_C, &ctrl10_c);
+
+  if (enable_state == EnableState::kEnabled) {
+    ctrl10_c |= 0b00000010;  // Set the TIMESTAMP_EN bit
+  } else {
+    ctrl10_c &= ~0b00000010;  // Clear the TIMESTAMP_EN bit
+  }
+
+  return WriteRegister(Register::CTRL10_C, &ctrl10_c);
+}
+
+bool LSM6DSV::EnableTimestampRounding(EnableState enable_state) {
+  uint8_t ctrl10_c;
+  ReadRegister(Register::CTRL10_C, &ctrl10_c);
+
+  if (enable_state == EnableState::kEnabled) {
+    ctrl10_c |= 0b00000100;  // Set the ROUNDING_STATUS bit
+  } else {
+    ctrl10_c &= ~0b00000100;  // Clear the ROUNDING_STATUS bit
+  }
+
+  return WriteRegister(Register::CTRL10_C, &ctrl10_c);
+}
+
+bool LSM6DSV::WriteGyroDataRate(LSM6DSV::GyroDataRate data_rate) {
+  uint8_t ctrl2_g;
+  ReadRegister(Register::CTRL2_G, &ctrl2_g);
+
+  // Clear the data rate bits
+  ctrl2_g &= ~0b11110000;
+
+  // Set the new data rate
+  ctrl2_g |= (static_cast<uint8_t>(data_rate) & 0b1111) << 4;
+
+  return WriteRegister(Register::CTRL2_G, &ctrl2_g);
+}
+
+bool LSM6DSV::WriteGyroFullScale(LSM6DSV::GyroFullScale full_scale) {
+  uint8_t ctrl2_g;
+  ReadRegister(Register::CTRL2_G, &ctrl2_g);
+
+  // Clear the full scale bits
+  ctrl2_g &= ~0b00001111;
+
+  // Set the new full scale
+  ctrl2_g |= static_cast<uint8_t>(full_scale) & 0b1111;
+
+  return WriteRegister(Register::CTRL2_G, &ctrl2_g);
+}
+
+LSM6DSV::GyroFullScale LSM6DSV::ReadGyroFullScale() {
+  uint8_t ctrl2_g;
+  ReadRegister(Register::CTRL2_G, &ctrl2_g);
+  return static_cast<GyroFullScale>(ctrl2_g & 0b00001111);
+}
+
+void LSM6DSV::ConfigureGyroSensitivity(GyroFullScale full_scale) {
+  switch (full_scale) {
+    case GyroFullScale::k125dps:
+      gyro_sensitivity_ = 0.004375f;  // Sensitivity for 125 dps
+      break;
+    case GyroFullScale::k250dps:
+      gyro_sensitivity_ = 0.00875f;  // Sensitivity for 250 dps
+      break;
+    case GyroFullScale::k500dps:
+      gyro_sensitivity_ = 0.0175f;  // Sensitivity for 500 dps
+      break;
+    case GyroFullScale::k1000dps:
+      gyro_sensitivity_ = 0.035f;  // Sensitivity for 1000 dps
+      break;
+    case GyroFullScale::k2000dps:
+      gyro_sensitivity_ = 0.07f;  // Sensitivity for 2000 dps
+      break;
+    case GyroFullScale::k4000dps:
+      gyro_sensitivity_ = 0.14f;  // Sensitivity for 4000 dps
+      break;
+    default:
+      gyro_sensitivity_ = 0.0f;  // Invalid full scale
+      break;
+  }
+}
+
+bool LSM6DSV::WriteAccDataRate(AccelDataRate data_rate) {
+  uint8_t ctrl1_xl;
+  ReadRegister(Register::CTRL1_XL, &ctrl1_xl);
+
+  // Clear the data rate bits
+  ctrl1_xl &= ~0b11110000;
+
+  // Set the new data rate
+  ctrl1_xl |= (static_cast<uint8_t>(data_rate) & 0b1111) << 4;
+
+  return WriteRegister(Register::CTRL1_XL, &ctrl1_xl);
+}
+
+bool LSM6DSV::WriteAccFullScale(LSM6DSV::AccelFullScale full_scale) {
+  uint8_t ctrl1_xl;
+  ReadRegister(Register::CTRL1_XL, &ctrl1_xl);
+
+  // Clear the full scale bits
+  ctrl1_xl &= ~0b00001100;
+
+  // Set the new full scale
+  ctrl1_xl |= (static_cast<uint8_t>(full_scale) & 0b11) << 2;
+
+  return WriteRegister(Register::CTRL1_XL, &ctrl1_xl);
+}
+
+LSM6DSV::AccelFullScale LSM6DSV::ReadAccFullScale() {
+  uint8_t ctrl1_xl;
+  ReadRegister(Register::CTRL1_XL, &ctrl1_xl);
+  return static_cast<AccelFullScale>((ctrl1_xl & 0b00001100) >> 2);
+}
+
+void LSM6DSV::ConfigureAccSensitivity(AccelFullScale full_scale) {
+  switch (full_scale) {
+    case AccelFullScale::k2g:
+      acc_sensitivity_ = 0.061f;  // Sensitivity for 2g
+      break;
+    case AccelFullScale::k4g:
+      acc_sensitivity_ = 0.122f;  // Sensitivity for 4g
+      break;
+    case AccelFullScale::k8g:
+      acc_sensitivity_ = 0.244f;  // Sensitivity for 8g
+      break;
+    case AccelFullScale::k16g:
+      acc_sensitivity_ = 0.488f;  // Sensitivity for 16g
+      break;
+    default:
+      acc_sensitivity_ = 0.0f;  // Invalid full scale
+      break;
+  }
+}
+
+bool LSM6DSV::ReadAccAndGyro(float& gyro_x, float& gyro_y, float& gyro_z,
+                             float& acc_x, float& acc_y, float& acc_z) {
+  uint8_t data[12];
+  bool status = ReadRegisters(Register::OUTX_L_G, data, 12);
+  if (!status) { return status; }
+  // Combine the low and high bytes for gyro and accelerometer data
+  gyro_x = static_cast<int16_t>((data[1] << 8) | data[0]) * gyro_sensitivity_;
+  gyro_y = static_cast<int16_t>((data[3] << 8) | data[2]) * gyro_sensitivity_;
+  gyro_z = static_cast<int16_t>((data[5] << 8) | data[4]) * gyro_sensitivity_;
+
+  acc_x = static_cast<int16_t>((data[7] << 8) | data[6]) * acc_sensitivity_;
+  acc_y = static_cast<int16_t>((data[9] << 8) | data[8]) * acc_sensitivity_;
+  acc_z = static_cast<int16_t>((data[11] << 8) | data[10]) * acc_sensitivity_;
+
+  return true;
+}
+
+bool LSM6DSV::ResetMemory() {
+  uint8_t ctrl3_c;
+  ReadRegister(Register::CTRL3_C, &ctrl3_c);
+
+  // Set the BOOT bit
+  ctrl3_c |= 0b1000000;
+
+  // Write back to the CTRL3_C register
+  return WriteRegister(Register::CTRL3_C, &ctrl3_c);
+}
+
+bool LSM6DSV::RebootDevice() {
+  uint8_t ctrl3_c;
+  ReadRegister(Register::CTRL3_C, &ctrl3_c);
+
+  // Set the SW_RESET bit
+  ctrl3_c |= 0b00000001;
+
+  // Write back to the CTRL3_C register
+  return WriteRegister(Register::CTRL3_C, &ctrl3_c);
+}
+
+bool LSM6DSV::WriteInt1Enable(uint8_t int_enable_mask) {
+  uint8_t reg;
+  ReadRegister(Register::INT1_CTRL, &reg);
+  reg |= int_enable_mask;
+  return WriteRegister(Register::INT1_CTRL, &reg);
+}
+
+bool LSM6DSV::WriteInt2Enable(uint8_t int_enable_mask) {
+  uint8_t reg;
+  ReadRegister(Register::INT2_CTRL, &reg);
+  reg |= int_enable_mask;
+  return WriteRegister(Register::INT2_CTRL, &reg);
+}
+
+// bool LSM6DSV::WriteSflpEnabled(EnableState enable_state) {
+//   uint8_t ctrl10_c;
+//   ReadRegister(Register::CTRL10_C, &ctrl10_c);
+
+//   if (enable_state == EnableState::kEnabled) {
+//     ctrl10_c |= 0b00001000;  // Set SLP_EN bit
+//   } else {
+//     ctrl10_c &= ~0b00001000;  // Clear SLP_EN bit
+//   }
+
+//   return WriteRegister(Register::CTRL10_C, &ctrl10_c);
+// }
+
+// bool LSM6DSV::WriteSflpOutput(SflpOutput output) {
+//   uint8_t ctrl10_c;
+//   ReadRegister(Register::SFLP_ODR, &ctrl10_c);
+
+//   ctrl10_c &= ~0b00110000;  // Clear SLP_OUT_SEL bits
+//   ctrl10_c |= (static_cast<uint8_t>(output) & 0b11) << 4;
+
+//   return WriteRegister(Register::SFLP_ODR, &ctrl10_c);
+// }
+
+// bool LSM6DSV::WriteSflpDataRate(SflpDataRate rate) {
+//   uint8_t sflp_odr;
+//   ReadRegister(Register::SFLP_ODR, &sflp_odr);
+
+//   sflp_odr &= ~0b00111000;  // Clear SLP_ODR bits
+//   sflp_odr |= (static_cast<uint8_t>(rate) & 0b111) << 3;
+
+//   return WriteRegister(Register::SFLP_ODR, &sflp_odr);
+// }
+
+// bool LSM6DSV::ReadQuaternion(float& q0, float& q1, float& q2, float& q3) {
+//   uint8_t data[16];
+//   if (!ReadRegisters(Register::QUATERNION_W_L, data, 16)) {
+//     return false;
+//   }
+//   int16_t qw = static_cast<int16_t>((data[1] << 8) | data[0]);
+//   int16_t qx = static_cast<int16_t>((data[3] << 8) | data[2]);
+//   int16_t qy = static_cast<int16_t>((data[5] << 8) | data[4]);
+//   int16_t qz = static_cast<int16_t>((data[7] << 8) | data[6]);
+
+//   // Scale factor depends on device settings, assume 2^-14 here
+//   constexpr float scale = 1.0f / 16384.0f;
+
+//   q0 = qw * scale;
+//   q1 = qx * scale;
+//   q2 = qy * scale;
+//   q3 = qz * scale;
+
+//   return true;
+// }
+
+// bool LSM6DSV::ReadGravity(float& gx, float& gy, float& gz) {
+//   uint8_t data[6];
+//   if (!ReadRegisters(Register::GRV_X_L, data, 6)) {
+//     return false;
+//   }
+//   int16_t x = static_cast<int16_t>((data[1] << 8) | data[0]);
+//   int16_t y = static_cast<int16_t>((data[3] << 8) | data[2]);
+//   int16_t z = static_cast<int16_t>((data[5] << 8) | data[4]);
+
+//   // Scale factor depends on device settings, assume 1/1000 as example
+//   constexpr float scale = 1.0f / 1000.0f;
+
+//   gx = x * scale;
+//   gy = y * scale;
+//   gz = z * scale;
+
+//   return true;
+// }
+
+// bool LSM6DSV::ReadGyroBias(float& bx, float& by, float& bz) {
+//   uint8_t data[6];
+//   if (!ReadRegisters(Register::GY_BIAS_X_L, data, 6)) {
+//     return false;
+//   }
+//   int16_t x = static_cast<int16_t>((data[1] << 8) | data[0]);
+//   int16_t y = static_cast<int16_t>((data[3] << 8) | data[2]);
+//   int16_t z = static_cast<int16_t>((data[5] << 8) | data[4]);
+
+//   // Scale factor depends on device settings, assume 1/1000 as example
+//   constexpr float scale = 1.0f / 1000.0f;
+
+//   bx = x * scale;
+//   by = y * scale;
+//   bz = z * scale;
+
+//   return true;
+// }
+
+}  // namespace lsm6dsv_lib
